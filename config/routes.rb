6a9b5bac12da1a9f8b6d4dea33065c32dcb5e3a4
KC <weilihmen@gmail.com>
2018-04-21 18:03:32 +0800

    routes default

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+	root "posts#index"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show] do
+	  member do
+	  	get :post
+      get :collect
+      get :comment
+      get :draft
+      get :friend
+    end
+  end
+  resources :posts
+	get "/feeds", to: "feeds#index"
 end
