384d5e523d74ca5ba2f686154b2e8854915c8ea6
KC <weilihmen@gmail.com>
2018-04-22 23:22:34 +0800

    fix User強制要求name

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   mount_uploader :avatar, PhotoUploader
+  validates_presence_of(:name)
 end
