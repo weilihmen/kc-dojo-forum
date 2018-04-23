bfc2d51a74017a6137f6811f7db2b58c50e7f7e5
KC <weilihmen@gmail.com>
2018-04-24 00:30:25 +0800

    fix request.referer issue

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,12 +10,12 @@ class PostsController < ApplicationController
 	def new
 		@post=Post.new
     @categories=Category.all
-		session[:return_to] ||= request.referer
+		session[:return_to_a] ||= request.referer
 		#https://stackoverflow.com/questions/2139996/how-to-redirect-to-previous-page-in-ruby-on-rails
 	end
 
   def edit
-    session[:return_to] ||= request.referer
+    session[:return_to_b] ||= request.referer
   end
 
   def show
@@ -27,7 +27,7 @@ class PostsController < ApplicationController
     @post.user = current_user
     if @post.save
       flash[:notice] = "新增成功"
-      redirect_to session.delete(:return_to)
+      redirect_to session.delete(:return_to_a)
     else
       flash.now[:alert] = "新增失敗"
       render :new
@@ -37,7 +37,7 @@ class PostsController < ApplicationController
 	def update
     if @post.update(post_params)
       flash[:notice] = "更新成功"
-      redirect_to session.delete(:return_to)
+      redirect_to session.delete(:return_to_b)
     else
       flash.now[:alert] = "更新失敗"
       render :edit
