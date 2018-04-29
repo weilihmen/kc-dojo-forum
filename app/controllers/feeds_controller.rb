bd59d2f599fdae383a8a01f70ab1a0825ae42c34
KC <weilihmen@gmail.com>
2018-04-29 23:49:23 +0800

    完成Feeds Index

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,2 +1,6 @@
 class FeedsController < ApplicationController
+	def index
+		@most_comment_posts=Post.joins(:comments).group("posts.id").order("count(posts.id) DESC").limit(10)
+		@most_comment_users=User.joins(:comments).group("users.id").order("count(users.id) DESC").limit(10)
+	end
 end
