6b43fa76d975053f1db8deb1bb9bc82dd21c7256
KC <weilihmen@gmail.com>
2018-04-29 15:39:10 +0800

    後台:完成users index頁面與功能

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,18 @@
 class Admin::UsersController < Admin::BaseController
+	def index
+		@q =  User.all.order(id: :desc).search(params[:q])
+    @users = @q.result(distinct: true).paginate(:page => params[:page], :per_page => 20)
+	end
+	def auth
+		@user = User.find(params[:user_id])
+		@user.update(role: "admin")
+		flash[:notice] = "更動成功"
+		redirect_to admin_users_path
+	end
+	def unauth
+		@user = User.find(params[:user_id])
+		@user.update(role: "normal")
+		flash[:notice] = "更動成功"
+		redirect_to admin_users_path
+	end
 end
